2aa27c11c7a4277f5ea782dba55d5348c6aa9558
Erling Mårtensson <erling.martensson@gmail.com>
2021-05-17 19:22:22 +0200

    feature: Books drop down value is set correctly

--- src/Book.js
+++ src/Book.js
@@ -6,7 +6,7 @@ export default function Book({details, onChange}) {
             <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${details.imageLinks && details.imageLinks.thumbnail})` }}></div>
                 <div className="book-shelf-changer">
-                    <select>
+                    <select defaultValue={details.shelf}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
